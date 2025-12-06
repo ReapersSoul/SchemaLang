0ede3903056922a2b3ea60f8cbcd7c05c476ae91
trevor <18trevor21@gmail.com>
2025-12-06 16:46:43 -0500

    Refactor SqliteGenerator: Remove unused utility and SQL generation functions
    - Deleted utility functions for combination generation.
    - Removed SQL string generation functions for creating tables, selecting, inserting, updating, and deleting records.
    - Cleaned up function declarations in SqliteGenerator.hpp and their implementations in SqliteGenerator.cpp.
    - Updated the escape_string function to maintain functionality.

--- Shared/include/BuiltInGenerators/SqliteGenerator.hpp
+++ Shared/include/BuiltInGenerators/SqliteGenerator.hpp
@@ -7,52 +7,6 @@ class SqliteGenerator : public Generator
 {
 	TypeDefinition sqlite_db = TypeDefinition("sqlite3 *");
 
-	// utility functions
-	std::vector<std::vector<int>> comb(int N, int K);
-
-	std::vector<std::vector<int>> comb(int N);
-
-	// sql string generation functions
-	std::string generate_create_table_statement_string_struct(std::shared_ptr<ProgramStructure> ps,StructDefinition &s);
-
-	std::string generate_select_all_statement_string_member_variable(StructDefinition &s, MemberVariableDefinition &mv);
-
-	std::vector<std::string> genrate_select_all_statements_string_struct(StructDefinition &s);
-
-	std::string generate_select_by_member_variable_statement_string(StructDefinition &s, MemberVariableDefinition &mv_1, std::vector<int> &criteria);
-
-	std::string generat_insert_statement_string_struct(StructDefinition &s);
-
-	std::string generate_update_all_statement_string_struct(StructDefinition &s);
-
-	std::string generate_delete_statement_string_struct(StructDefinition &s);
-
-	// functions for c++ code generation
-	void generate_select_all_statement_function_member_variable(std::shared_ptr<Generator>gen, std::shared_ptr<ProgramStructure>ps, StructDefinition &s, MemberVariableDefinition &mv);
-
-	void generate_select_all_statement_functions_struct(std::shared_ptr<Generator>gen, std::shared_ptr<ProgramStructure>ps, StructDefinition &s);
-
-	void generate_select_member_variable_function_statement(std::shared_ptr<Generator>gen, std::shared_ptr<ProgramStructure>ps, StructDefinition &s, MemberVariableDefinition &mv_1, std::vector<int> &criteria);
-
-	void generate_select_statements_function_struct(std::shared_ptr<Generator>gen, std::shared_ptr<ProgramStructure>ps, StructDefinition &s);
-
-	void generate_insert_statements_function_struct(std::shared_ptr<Generator>gen, std::shared_ptr<ProgramStructure>ps, StructDefinition &s);
-
-	void generate_update_all_statement_function_struct(std::shared_ptr<Generator>gen, std::shared_ptr<ProgramStructure>ps, StructDefinition &s);
-
-	void generate_update_statements_function_struct(std::shared_ptr<Generator>gen, std::shared_ptr<ProgramStructure>ps, StructDefinition &s);
-
-	void generate_delete_statement_function_struct(std::shared_ptr<Generator>gen, std::shared_ptr<ProgramStructure>ps, StructDefinition &s);
-
-	// file generation functions
-	bool generate_create_table_file(std::shared_ptr<ProgramStructure>ps, StructDefinition &s, std::string out_path);
-
-	bool generate_select_all_files(std::shared_ptr<ProgramStructure>ps, StructDefinition &s, std::string out_path);
-
-	bool generate_select_files(std::shared_ptr<ProgramStructure>ps, StructDefinition &s, std::string out_path);
-
-	bool generate_struct_files(std::shared_ptr<ProgramStructure>ps, StructDefinition &s, std::string out_path);
-
 	std::string escape_string(std::string str);
 
 	// Function to add foreign key columns for array relationships
